30fc3c183981d3fe5f6aba7353d84abee7c0681d
Nilak Pathak <nilakpathak14@gmail.com>
2025-01-04 17:20:13 +0545

    added expenses form

---
+++ MoneyMate/Models/Tags.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MoneyMate.Models
+{
+    public class TagsModel
+    {
+        public string tagId { get; set; }
+        public string tagName { get; set; }
+        public string tagDescription { get; set; }
+
+    }
+}
---
+++ MoneyMate/Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace MoneyMate.Models
+{
+    public class TransactionModel
+    {
+        public string title { get; set; }
+        public string transactionTagId { get; set; }
+        public string transactionId { get; set; }
+        public float amount { get; set; }
+        public string note { get; set; }
+        public DateTime transactionDate { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public TransactionType transactionType { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public  DebtStatus? debtStatus { get; set; } 
+        public string debtSource { get; set; }
+        public DateTime debtDueDate { get; set; }
+
+    }
+}
